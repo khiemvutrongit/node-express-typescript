252a739303cea831bd761f0f36cbbe520d5f2339
khiemvutrongit <95041406+khiemvutrongit@users.noreply.github.com>
2022-01-16 04:52:46 +0700

    remove file figlet-config & update test brands

--- src/tests/integration/brand.test.ts
+++ src/tests/integration/brand.test.ts
@@ -15,6 +15,7 @@ describe("MP08 /manage-product/v1/brands?query,sort,limit,page", () => {
 
     expect(response.status).toEqual(200);
   });
+
   test("query should by field name should return success", async () => {
     const response = await server
       .get("/manage-product/v1/brands?query=name%like%test");
@@ -24,34 +25,145 @@ describe("MP08 /manage-product/v1/brands?query,sort,limit,page", () => {
 });
 
 describe("MP09 /manage-product/v1/brands/private?query,sort,limit,page", () => {
-  test("should be return success", () => {});
-  test("missing header authorization should be return false", () => {});
-  test("query should by field name should return success", () => {})
+  const tokenMock = generateToken(payload, privateKey);
+
+  test("should be return success", async () => {
+    const response = await server
+      .get('/manage-product/v1/brands/private?query=name%eq%test&limit=1$start=0&sort=-createdAt')
+      .set('Authorization', `Bearer ${tokenMock}`);
+    
+    expect(response.status).toEqual(200);
+  });
+
+  test("missing header authorization should be return false", async () => {
+    const response = await server
+    .get('/manage-product/v1/brands/private?query=name%eq%test&limit=1$start=0,sort=-createdAt')
+  
+    expect(response.status).toEqual(401);
+  });
 });
 
 describe("MP10 /manage-product/v1/brands/{id}", () => {
-  test("get product public should return success", () => {});
-  test("invalid id should be return false", () => {});
+  test("get product public should return success", async () => {
+    const brandMock = await createBrandService(mockData.createBrandMock);
+    const response = await server
+    .get(`/manage-product/v1/brands/${brandMock.id}`);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test("invalid id should be return false", async () => {
+    const response = await server
+    .get(`/manage-product/v1/brands/${mockData.fakeId}`);
+
+    expect(response.status).toEqual(400);
+  });
 });
 
 describe("MP11 /manage-product/v1/brands/private/{id}", () => {
-  test("get product public should return success", () => {});
-  test("invalid id should be return false", () => {});
+  const tokenMock = generateToken(payload, privateKey);
+
+  test("get product public should return success", async () => {
+    const brandMock = await createBrandService(mockData.createBrandMock);
+    const response = await server
+      .get(`/manage-product/v1/brands/private/${brandMock.id}`)
+      .set('Authorization', `Bearer ${tokenMock}`);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test("invalid id should be return false", async () => {
+    const response = await server
+    .get(`/manage-product/v1/brands/private/${mockData.fakeId}`)
+    .set('Authorization', `Bearer ${tokenMock}`);
+
+  expect(response.status).toEqual(400);
+  });
 });
 
 describe("MP12 /manage-product/v1/brands", () => {
-  test("missing parameter body should return false", () => {});
-  test("should return success", () => {});
+  const tokenMock = generateToken(payload, privateKey);
+
+  test("missing parameter body should return false", async () => {
+    const response = await server
+      .post(`/manage-product/v1/brands`)
+      .set("Accept", "application/json")
+      .set('Authorization', `Bearer ${tokenMock}`)
+      .send({});
+      
+      expect(response.status).toEqual(400);
+  });
+
+  test("should return success", async () => {
+    const response = await server
+      .post(`/manage-product/v1/brands`)
+      .set("Accept", "application/json")
+      .set('Authorization', `Bearer ${tokenMock}`)
+      .send(mockData.createBrandMock);
+    
+    expect(response.status).toEqual(201);
+  });
 });
 
 describe("MP13 /manage-product/v1/brands/{id}", () => {
-  test("update brand should be success", () => {});
-  test("body product is empty should be false", () => {});
-  test("invalid id should return false", () => {});
+  const tokenMock = generateToken(payload, privateKey);
+
+  test("update brand should be success", async () => {
+    const brandMock = await createBrandService(mockData.createBrandMock);
+    const response = await server
+      .put(`/manage-product/v1/brands/${brandMock.id}`)
+      .set('Authorization', `Bearer ${tokenMock}`)
+      .set("Accept", "application/json")
+      .send(mockData.updateBrandMock);
+    
+    expect(response.status).toEqual(200);
+  });
+
+  test("body product is empty should be false", async () => {
+    const brandMock = await createBrandService(mockData.createBrandMock);
+    const response = await server
+      .put(`/manage-product/v1/brands/${brandMock.id}`)
+      .set('Authorization', `Bearer ${tokenMock}`)
+      .set("Accept", "application/json")
+      .send({});
+    
+    expect(response.status).toEqual(400);
+  });
+
+  test("invalid id should return false", async () => {
+    const response = await server
+      .put(`/manage-product/v1/brands/${mockData.fakeId}`)
+      .set('Authorization', `Bearer ${tokenMock}`)
+      .set("Accept", "application/json")
+      .send({});
+    
+    expect(response.status).toEqual(400);
+  });
 });
 
 describe("MP14 /manage-product/v1/brands/{id}", () => {
-  test("delete brand should be success", () => {});
-  test("missing header authorization should be return false", () => {});
-  test("invalid id should return false", () => {});
+  const tokenMock = generateToken(payload, privateKey);
+
+  test("delete brand should be success", async () => {
+    const brandMock = await createBrandService(mockData.createBrandMock);
+    const response = await server
+      .delete(`/manage-product/v1/brands/${brandMock.id}`)
+      .set('Authorization', `Bearer ${tokenMock}`)
+
+    expect(response.status).toEqual(204);
+  });
+  test("missing header authorization should be return false", async () => {
+    const brandMock = await createBrandService(mockData.createBrandMock);
+    const response = await server
+      .delete(`/manage-product/v1/brands/${brandMock.id}`);
+
+    expect(response.status).toEqual(401);
+  });
+  test("invalid id should return false", async () => {
+    const response = await server
+      .delete(`/manage-product/v1/brands/${mockData.fakeId}`)
+      .set('Authorization', `Bearer ${tokenMock}`)
+
+    expect(response.status).toEqual(400);
+  });
 });
