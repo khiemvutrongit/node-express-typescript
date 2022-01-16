2ada48c5b56f9d530f163985fc397ec4cb096512
khiemvutrongit <95041406+khiemvutrongit@users.noreply.github.com>
2022-01-16 14:29:13 +0700

    update copyfile

--- src/copyfile.js
+++ src/copyfile.js
@@ -3,13 +3,13 @@ const packageFile = require('./package.json');
 
 delete packageFile.devDependencies;
 delete packageFile.nodemonConfig;
-delete packageFile.dependencies.figlet;
 
 const packageObj = {
   ...packageFile,
+  main: "index.js",
   scripts: {
     start: "DEVELOP_ENV=prod node server.js",
-    test: "DEVELOP_ENV=dev jest --color"
+    test: "DEVELOP_ENV=dev jest --detectOpenHandles"
   }
 };
 
